81da9e7591d18f59abe08128b7475b8e71a88581
Phong Tran <aphtr001@edu.xamk.fi>
2020-10-07 19:03:49 +0300

    initiate graphql server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,22 @@
-import express, { Response } from 'express';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/Hello';
 
 const app = express();
 
-app.get('/', (_req, res: Response) => {
-  res.send({ hello: 'there' });
-});
+const main = async () => {
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false
+    }),
+    context: ({ req, res }) => ({ req, res })
+  });
+  apolloServer.applyMiddleware({ app });
+  app.listen(4000, () => {
+    console.log('Listening on port 4000');
+  });
+};
 
-app.listen(4000, () => {
-  console.log('Listening on port 4000');
-});
+main();
